74e1392e49d5f4dfc8b9d3342e22fc4b5761b9b8
Maxim L. <38497831+StableBig@users.noreply.github.com>
2024-05-25 18:48:38 +0200

    add views for event list, event detail and question creation

--- event_models/views.py
+++ event_models/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Event, Speaker, Question
+from .forms import EventForm, SpeakerForm, QuestionForm
 
-# Create your views here.
+def event_list(request):
+    events = Event.objects.all()
+    return render(request, 'event_list.html', {'events': events})
+
+def event_detail(request, pk):
+    event = get_object_or_404(Event, pk=pk)
+    return render(request, 'event_detail.html', {'event': event})
+
+def question_create(request):
+    if request.method == 'POST':
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('event_list')
+    else:
+        form = QuestionForm()
+    return render(request, 'question_form.html', {'form': form})
