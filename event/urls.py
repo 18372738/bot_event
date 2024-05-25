308915ea16a531f8ad8d7ff1dd44aea0c2787dc2
Maxim L. <38497831+StableBig@users.noreply.github.com>
2024-05-25 20:35:23 +0200

    update path in urls.py

--- event/urls.py
+++ event/urls.py
@@ -15,10 +15,9 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
-from event_models import views
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),  # Добавление корневого URL
+    path('', include('event_models.urls')),
 ]
