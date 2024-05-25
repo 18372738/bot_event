627f6944a6e505f238f86082077cd476085e5c9c
Maxim L. <38497831+StableBig@users.noreply.github.com>
2024-05-25 19:58:49 +0200

    Update views and urls

--- event_models/views.py
+++ event_models/views.py
@@ -1,21 +1,4 @@
-from django.shortcuts import render, get_object_or_404, redirect
-from .models import Event, Speaker, Question
-from .forms import EventForm, SpeakerForm, QuestionForm
+from django.http import HttpResponse
 
-def event_list(request):
-    events = Event.objects.all()
-    return render(request, 'event_list.html', {'events': events})
-
-def event_detail(request, pk):
-    event = get_object_or_404(Event, pk=pk)
-    return render(request, 'event_detail.html', {'event': event})
-
-def question_create(request):
-    if request.method == 'POST':
-        form = QuestionForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('event_list')
-    else:
-        form = QuestionForm()
-    return render(request, 'question_form.html', {'form': form})
+def home(request):
+    return HttpResponse("Welcome to the Event Management System!")
