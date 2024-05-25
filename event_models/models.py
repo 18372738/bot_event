261d4fc908a1d1a685566bb5048ce51a4d43b397
Maxim L. <38497831+StableBig@users.noreply.github.com>
2024-05-25 19:38:12 +0200

    Add telegram_id field to Speaker model

--- event_models/models.py
+++ event_models/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 
 class Speaker(models.Model):
+    """Спикер"""
     full_name = models.CharField("ФИО", max_length=200)
     topic = models.CharField("Тема доклада", max_length=200)
     phone_number = models.CharField("Номер телефона", max_length=20, null=True, blank=True)
@@ -9,12 +10,13 @@ class Speaker(models.Model):
     end_at = models.DateTimeField("Время окончания")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    telegram_id = models.CharField("Telegram ID", max_length=50, null=True, blank=True)
 
     def __str__(self):
         return self.full_name
 
-
 class Event(models.Model):
+    """Мероприятие"""
     title = models.CharField("Название", max_length=200)
     address = models.CharField("Адрес", max_length=200)
     start_at = models.DateTimeField("Время начала")
@@ -26,8 +28,8 @@ class Event(models.Model):
     def __str__(self):
         return self.title
 
-
 class Question(models.Model):
+    """Вопрос"""
     speaker = models.ForeignKey(Speaker, verbose_name="Спикер", on_delete=models.CASCADE, related_name="questions")
     question = models.TextField("Вопрос")
     answer = models.TextField("Ответ", blank=True)
