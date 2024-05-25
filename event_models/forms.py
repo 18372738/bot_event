591b4fe157ab335f93b77c1eb6515a5b23f65e03
Maxim L. <38497831+StableBig@users.noreply.github.com>
2024-05-25 21:06:11 +0200

    update forms.py for NewSpeaker model

--- event_models/forms.py
+++ event_models/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Event, Speaker, Question
+from .models import Event, Speaker, Question, NewSpeaker
 
 class EventForm(forms.ModelForm):
     class Meta:
@@ -15,3 +15,8 @@ class QuestionForm(forms.ModelForm):
     class Meta:
         model = Question
         fields = ['speaker', 'question', 'answer']
+
+class NewSpeakerForm(forms.ModelForm):
+    class Meta:
+        model = NewSpeaker
+        fields = ['full_name', 'topic', 'phone_number', 'telegram_id']
