8df3ec8d4eb4dde9dc6e3103472c2d055a97e092
Maxim L. <38497831+StableBig@users.noreply.github.com>
2024-05-25 16:48:48 +0200

    add auto_now_add and auto_now in Models

--- event_models/models.py
+++ event_models/models.py
@@ -7,8 +7,8 @@ class Speaker(models.Model):
     phone_number = models.CharField("Номер телефона", max_length=20, null=True, blank=True)
     start_at = models.DateTimeField("Время начала")
     end_at = models.DateTimeField("Время окончания")
-    created_at = models.DateTimeField(default=timezone.now)
-    updated_at = models.DateTimeField(default=timezone.now)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.full_name
@@ -20,8 +20,8 @@ class Event(models.Model):
     start_at = models.DateTimeField("Время начала")
     end_at = models.DateTimeField("Время окончания")
     speakers = models.ManyToManyField(Speaker, verbose_name="Спикеры", related_name="events")
-    created_at = models.DateTimeField(default=timezone.now)
-    updated_at = models.DateTimeField(default=timezone.now)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
@@ -31,8 +31,8 @@ class Question(models.Model):
     speaker = models.ForeignKey(Speaker, verbose_name="Спикер", on_delete=models.CASCADE, related_name="questions")
     question = models.TextField("Вопрос")
     answer = models.TextField("Ответ", blank=True)
-    created_at = models.DateTimeField(default=timezone.now)
-    updated_at = models.DateTimeField(default=timezone.now)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"Question to {self.speaker.full_name}"
