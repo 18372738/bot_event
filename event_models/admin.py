e0e5d85aa28bd60dd57daeb41d2eaab8b262fae5
Maxim L. <38497831+StableBig@users.noreply.github.com>
2024-05-25 20:34:33 +0200

    update path in admin.py

--- event_models/admin.py
+++ event_models/admin.py
@@ -3,18 +3,18 @@ from .models import Event, Speaker, Question
 
 @admin.register(Event)
 class EventAdmin(admin.ModelAdmin):
-    list_display = ('title', 'start_at', 'end_at', 'created_at', 'updated_at')
+    list_display = ('title', 'start_at', 'end_at', 'created_on', 'updated_on')
     search_fields = ('title',)
     list_filter = ('start_at', 'end_at')
 
 @admin.register(Speaker)
 class SpeakerAdmin(admin.ModelAdmin):
-    list_display = ('full_name', 'topic', 'start_at', 'end_at', 'created_at', 'updated_at', 'telegram_id')
+    list_display = ('full_name', 'topic', 'start_at', 'end_at', 'created_on', 'updated_on', 'telegram_id')
     search_fields = ('full_name', 'topic')
     list_filter = ('start_at', 'end_at')
 
 @admin.register(Question)
 class QuestionAdmin(admin.ModelAdmin):
-    list_display = ('speaker', 'question', 'answer', 'created_at', 'updated_at')
+    list_display = ('speaker', 'question', 'answer', 'created_on', 'updated_on')
     search_fields = ('speaker__full_name', 'question', 'answer')
-    list_filter = ('created_at', 'updated_at')
+    list_filter = ('created_on', 'updated_on')
