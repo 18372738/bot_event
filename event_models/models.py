fbbe967a3f9802a3e5ba809e541be180eb13f7c4
Maxim L. <38497831+StableBig@users.noreply.github.com>
2024-05-25 16:40:36 +0200

    add default values for created_at and updated_at

--- event_models/models.py
+++ event_models/models.py
@@ -1,16 +1,15 @@
 from django.db import models
-
+from django.utils import timezone
 
 class Speaker(models.Model):
     """Спикер"""
-
     full_name = models.CharField("ФИО", max_length=200)
     topic = models.CharField("Тема доклада", max_length=200)
     phone_number = models.CharField("Номер телефона", max_length=20, null=True, blank=True)
     start_at = models.DateTimeField("Время начала")
     end_at = models.DateTimeField("Время окончания")
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True, default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True, default=timezone.now)
 
     def __str__(self):
         return self.full_name
@@ -18,14 +17,13 @@ class Speaker(models.Model):
 
 class Event(models.Model):
     """Мероприятие"""
-
     title = models.CharField("Название", max_length=200)
     address = models.CharField("Адрес", max_length=200)
     start_at = models.DateTimeField("Время начала")
     end_at = models.DateTimeField("Время окончания")
     speakers = models.ManyToManyField(Speaker, verbose_name="Спикеры", related_name="events")
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True, default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True, default=timezone.now)
 
     def __str__(self):
         return self.title
@@ -33,12 +31,11 @@ class Event(models.Model):
 
 class Question(models.Model):
     """Вопрос"""
-
     speaker = models.ForeignKey(Speaker, verbose_name="Спикер", on_delete=models.CASCADE, related_name="questions")
     question = models.TextField("Вопрос")
     answer = models.TextField("Ответ", blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True, default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True, default=timezone.now)
 
     def __str__(self):
         return f"Question to {self.speaker.full_name}"
